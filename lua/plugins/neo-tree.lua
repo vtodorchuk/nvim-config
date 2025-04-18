d3a7dd9c525f73be1f9e595f9bc8b3222d817a6b
vtodorchuk <bozodee8@gmail.com>
2025-04-18 17:14:34 +0300

    New theme

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -3,8 +3,18 @@ return {
   branch = "v3.x",
   dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", "MunifTanjim/nui.nvim" },
   config = function()
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          visible = true,  -- Show hidden files
+          hide_dotfiles = false,  -- Do not hide dotfiles
+          hide_gitignored = false,  -- Optional: show gitignored files too
+        },
+      },
+    })
+
     vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>')
-    vim.keymap.set('n', '<C-m>', ':Neotree close')
+    vim.keymap.set('n', '<C-m>', ':Neotree close<CR>')
   end
 }
 
